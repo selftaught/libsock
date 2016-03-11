769e2d5c7d96bfae931613d3da90709d46888ea2
Dillan Hildebrand <88502c@bluehost.com>
2016-03-10 22:36:01 -0700

    moving all sockit code to sockit.hpp, removing tcp/udp classes, implementing templating of Socket class

--- libsockit/main.cpp
+++ libsockit/main.cpp
@@ -10,22 +10,40 @@
 
 int main(int argc, char** argv) {
     
-    TcpSocket socket(9999);
-    socket.set_service_type(SERVER);
-    socket.connect();
-    
+    Socket<TCP, SERVER>* socket = new Socket<TCP, SERVER>(9999);
+    socket->connect();
+
     while(true) {
-        try {
-            std::string received = socket.receive();
-            
-            if(!received.empty()) {
-                std::cout << received << std::endl;
+        
+        int e = poll(socket->pfd(), 1, 10000);
+        
+        switch(e) {
+            case POLL_EXPIRE: {
+                //std::cout << "Poll timed out\n";
+                break;
+            }
+                
+            case POLL_ERR: {
+                //std::cout << std::strerror(errno) << std::endl;
+                break;
+            }
+                
+            default: {
+                try {
+                    std::string received = socket->receive();
+                    
+                    if(!received.empty()) {
+                        std::cout << received << std::endl;
+                    }
+                }
+                catch(SocketException e) {
+                    std::cout << e.what() << std::endl;
+                    break;
+                }
             }
         }
-        catch(SocketException e) {
-            std::cout << e.what() << std::endl;
-            break;
-        }
+        
+        sleep(1);
     }
     
     return EXIT_SUCCESS;
--- libsockit/sockit.hpp
+++ libsockit/sockit.hpp
@@ -64,6 +64,11 @@
 #endif
 
 /**
+ *
+ */
+#define POLL_EXPIRE (0)
+
+/**
  * Default recv char array lengths
  * for the UDP and TCP protocols.
  */
@@ -78,15 +83,9 @@
 #define ERR_BUF_LEN 256
 
 /**
- *
- */
-#define DEFAULT_TV_SEC  5
-#define DEFAULT_TV_USEC 0
-
-/**
  * Not really neccessary because IPPROTO_TCP and IPPROTO_UDP
  * are both 0 but, meh. This function macro gets used in 
- * SocketBase::connect_client and SocketBase::connect_server.
+ * Socket::connect_client and Socket::connect_server.
  */
 #define IPPROTO(TYPE) ((TYPE) == (SOCK_STREAM) ? (IPPROTO_TCP) : (IPPROTO_UDP))
 
@@ -94,6 +93,14 @@ enum SERVICE_TYPE {
     UNDEF,
     CLIENT,
     SERVER
+};
+
+enum SOCKET_TYPE {
+    TCP = SOCK_STREAM,
+    UDP = SOCK_DGRAM,
+    RAW = SOCK_RAW,
+    SEQ = SOCK_SEQPACKET,
+    RDM = SOCK_RDM
 };
 
 enum IP {
@@ -104,7 +111,7 @@ enum IP {
 /**
  * Define the default socket value. This is done because without
  * it, we'd have to add preprocessor conditions to each constructor
- * of SocketBase checking and defining m_socket based on the current
+ * of Socket checking and defining m_socket based on the current
  * system. However, by define DEFAULT_SOCKET_VAL, we don't need to do that.
  * We can simply define m_socket like m_socket(DEFAULT_SOCKET_VAL) instead.
  */
@@ -182,7 +189,8 @@ protected:
  * This is the class which UdpSocket and TcpSocket are derived from.
  *
  */
-class SocketBase {
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+class Socket {
 protected:
     
     /**
@@ -218,7 +226,7 @@ protected:
     /**
      * Type (SOCK_STREAM or SOCK_DGRAM).
      */
-    int m_type;
+    SOCKET_TYPE m_type;
 
     /**
      * Maximum backlog size.
@@ -238,7 +246,7 @@ protected:
     /**
      *
      */
-    struct pollfd m_pfd;
+    struct pollfd* m_pfd;
     
     /**
      * If the current system is *nix or apple
@@ -277,171 +285,478 @@ public:
     /**
      * Constructors.
      */
-    SocketBase(const int& type, uint16_t buf_size):
+    Socket(const uint16_t& port):
 		m_socket(DEFAULT_SOCKET_VAL),
-        m_port(0),
+        m_port(port),
         m_af(AF_INET),
-        m_type(type),
+        m_type(socket_t),
         m_backlog(5),
-        m_buf_size(buf_size),
-        m_service_type(SERVER) {
-            //m_time.tv_sec  = DEFAULT_TV_SEC;
-            //m_time.tv_usec = DEFAULT_TV_USEC;
-        }
-    
-    SocketBase(const uint16_t& port, const int& type, uint16_t buf_size):
-        m_socket(DEFAULT_SOCKET_VAL),
-		m_port(port),
-        m_af(AF_INET),
-        m_type(type),
-        m_backlog(5),
-        m_buf_size(buf_size),
-        m_service_type(SERVER) {
-            //m_time.tv_sec  = DEFAULT_TV_SEC;
-            //m_time.tv_usec = DEFAULT_TV_USEC;
-        }
-    
-    SocketBase(const std::string& hostname, const uint16_t& port, const int& type, uint16_t buf_size):
+        m_service_type(service_t),
+        m_pfd(nullptr)
+    { }
+
+    Socket(const std::string& hostname, const uint16_t& port):
 		m_socket(DEFAULT_SOCKET_VAL),
         m_hostname(hostname),
         m_port(port),
         m_af(AF_INET),
-        m_type(type),
+        m_type(socket_t),
         m_backlog(5),
-        m_buf_size(buf_size),
-        m_service_type(CLIENT) {
-            //m_time.tv_sec  = DEFAULT_TV_SEC;
-            //m_time.tv_usec = DEFAULT_TV_USEC;
-        }
+        m_service_type(service_t),
+        m_pfd(nullptr)
+    { }
     
-    SocketBase(const std::string&, const int&, uint16_t);
-    SocketBase(const std::string&, const std::string&, const int&, uint16_t);
+    Socket(const std::string&, uint16_t);
+    Socket(const std::string&, const std::string&, uint16_t);
     
     /**
      * Destructor.
      */
-    ~SocketBase();
+    ~Socket();
     
     /**
      * Prototypes.
      */
+    
+    void init();
     void disconnect();
     void connect();
+    void send(const std::string&);
     
     bool ready(const uint32_t&);
-    bool set_blocking(int, bool);
+    bool set_blocking(int, bool blocking = false);
     
-    virtual void send(const std::string&) = 0;
-    virtual std::string receive() = 0;
+    std::string receive();
     
     /**
      * Setters
      */
-    void set_hostname(const std::string& hostname) {
-        m_hostname = hostname;
-    }
-    
-    void set_port(const uint16_t& port) {
-        m_port = port;
-    }
-    
-    void set_port(const std::string& port) {
-        m_port = atoi(port.c_str());
-    }
-    
-    void set_address_family(const int& address_family) {
-        m_af = address_family;
-    }
-    
-    void set_type(const int& type) {
-        m_type = type;
-    }
-    
-    void set_backlog(const uint32_t& backlog) {
-        m_backlog = backlog;
-    }
-    
-    void set_ipv4() {
-        m_ip_version = V4;
-    }
-    
-    void set_ipv6() {
-        m_ip_version = V6;
-    }
+    void set_hostname      (const std::string& hostname) { m_hostname   = hostname;           }
+    void set_port          (const uint16_t&    port    ) { m_port       = port;               }
+    void set_port          (const std::string& port    ) { m_port       = atoi(port.c_str()); }
+    void set_address_family(const int&         addrf   ) { m_af         = addrf;              }
+    void set_backlog       (const uint32_t&    backlog ) { m_backlog    = backlog;            }
+    void set_ipv4          (                           ) { m_ip_version = V4;                 }
+    void set_ipv6          (                           ) { m_ip_version = V6;                 }
     
     /**
-     * Explicitly declare the service type (CLIENT or SERVER)
-     * despite the fact the service type get's intelligently
-     * set in the constructor.
-     */
-    void set_service_type(const SERVICE_TYPE& type) {
-        m_service_type = type;
-    }
-};
-
-/**
- * TCP (stream) socket class.
- */
-class TcpSocket : public SocketBase {
-public:
-    /**
-     * Constructors
      *
-     * Default m_recv_buf_size is because the MTU (Max Transmission Unit)
-     * for ethernet is TCP_RECV_BUF_LEN bytes.
      */
-    TcpSocket(const uint16_t& port, uint16_t buf_size = TCP_RECV_BUF_LEN):
-    SocketBase(port, SOCK_STREAM, buf_size)
-    { }
-    
-    TcpSocket(const std::string& hostname, const uint16_t& port, uint16_t buf_size = TCP_RECV_BUF_LEN):
-    SocketBase(hostname, port, SOCK_STREAM, buf_size)
-    { }
-    
-    TcpSocket(const std::string& port):
-    SocketBase(port, SOCK_STREAM, TCP_RECV_BUF_LEN)
-    { }
-    
-    TcpSocket(const std::string& hostname, const std::string& port, uint16_t buf_size = TCP_RECV_BUF_LEN):
-    SocketBase(hostname, port, SOCK_STREAM, buf_size)
-    { }
-    
-    void set_buf_size(const uint16_t& size) {
-        m_buf_size = size;
+    pollfd* pfd() {
+        return m_pfd;
     }
-    
-    void send(const std::string&);
-    
-    std::string receive();
 };
 
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+Socket<socket_t, service_t>::Socket(const std::string& port, uint16_t buf_size) {
+    m_socket	   = DEFAULT_SOCKET_VAL;
+    m_port         = atoi(port.c_str());
+    m_af           = AF_INET;
+    m_type         = socket_t;
+    m_backlog      = 5;
+    m_buf_size     = buf_size;
+    m_service_type = service_t;
+    m_pfd          = nullptr;
+}
+
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+Socket<socket_t, service_t>::Socket(const std::string& hostname, const std::string& port, uint16_t buf_size) {
+    m_socket	   = DEFAULT_SOCKET_VAL;
+    m_hostname     = hostname;
+    m_port         = atoi(port.c_str());
+    m_af           = AF_INET;
+    m_type         = socket_t;
+    m_backlog      = 5;
+    m_buf_size     = buf_size;
+    m_service_type = service_t;
+    m_pfd          = nullptr;
+}
+
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+Socket<socket_t, service_t>::~Socket() {
+#if defined(__NIX)
+    
+#else
+    
+#endif
+    disconnect();
+}
+
 /**
- * UDP (datagram) socket class.
+ * Calls the correct connect function (server or client)
+ * based on the value of m_service_type;
  */
-class UdpSocket : public SocketBase {
-public:
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+void Socket<socket_t, service_t>::connect() {
+    
     /**
-     * Constructors
+     * TCP specific declarations
      */
-    UdpSocket(const uint16_t& port, uint16_t buf_size = UDP_RECV_BUF_LEN):
-    SocketBase(port, SOCK_DGRAM, buf_size)
-    { }
+    if(m_type == TCP) {
+        m_buf_size = TCP_RECV_BUF_LEN;
+    }
+    /**
+     * UDP specific declarations
+     */
+    else if(m_type == UDP) {
+        m_buf_size = UDP_RECV_BUF_LEN;
+    }
     
-    UdpSocket(const std::string& hostname, const uint16_t& port, uint16_t buf_size = UDP_RECV_BUF_LEN):
-    SocketBase(hostname, port, SOCK_DGRAM, buf_size)
-    { }
+    if(m_pfd != nullptr) {
+        delete m_pfd;
+    }
     
-    UdpSocket(const std::string& port):
-    SocketBase(port, SOCK_DGRAM, UDP_RECV_BUF_LEN)
-    { }
+    m_pfd = (pollfd*)malloc(sizeof(pollfd));
     
-    UdpSocket(const std::string& hostname, const std::string& port, uint16_t buf_size = UDP_RECV_BUF_LEN):
-    SocketBase(hostname, port, SOCK_DGRAM, buf_size)
-    { }
+    if(m_service_type == UNDEF) {
+        throw SocketException("service_type_not_specified");
+    }
     
-    void send(const std::string&);
+    if(m_service_type == SERVER) { connect_server(); }
+    else if(m_service_type == CLIENT) { connect_client(); }
+    else {
+        throw SocketException("invalid_service_type");
+    }
+}
+
+/**
+ * Closes the socket if it's open.
+ */
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+void Socket<socket_t, service_t>::disconnect() {
     
-    std::string receive();
-};
+    if(m_pfd != nullptr) {
+        delete m_pfd;
+    }
+    
+#if defined(__NIX)
+    if(m_socket != -1) {
+        close(m_socket);
+    }
+#else
+    if (m_result != NULL) {
+        freeaddrinfo(m_result);
+    }
+    
+    WSACleanup();
+#endif
+}
+
+/**
+ * Does the needful to setup a server socket.
+ */
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+void Socket<socket_t, service_t>::connect_server() {
+    /**
+     * Throw an exception if the port hasn't be defined by the user.
+     */
+    if(!m_port) {
+        throw SocketException("port_not_defined");
+    }
+    
+#if defined(__NIX)
+    m_socket = socket(m_af, m_type, IPPROTO(m_type));
+    
+    /**
+     * Throw an exception if the socket connection failed.
+     */
+    if(m_socket == -1) {
+        throw SocketException("socket_failed: %s", strerror(errno));
+    }
+    
+    /**
+     * Set all bytes of m_host to zero. memset() is MT-Safe
+     * See: http://man7.org/linux/man-pages/man3/memset.3.html
+     */
+    memset(m_host, sizeof(struct sockaddr_in), 0);
+    
+    /**
+     * Connection info.
+     */
+    m_sockaddr.sin_family      = m_af;
+    m_sockaddr.sin_addr.s_addr = INADDR_ANY;
+    m_sockaddr.sin_port        = htons(m_port);
+    
+    /**
+     * If binding fails, throw an exception.
+     */
+    if(bind(m_socket, (struct sockaddr*)&m_sockaddr, sizeof(struct sockaddr_in)) == -1) {
+        close(m_socket);
+        throw SocketException("bind_failed: %s", strerror(errno));
+    }
+    
+    /**
+     * If this is a TCP server then we need to
+     * put the socket in listening mode.
+     */
+    if(m_type == SOCK_STREAM) {
+        listen(m_socket, m_backlog);
+    }
+    
+#else
+    /**
+     * Initiate the use of Winsock DLL
+     */
+    int err = WSAStartup(MAKEWORD(2, 2), &m_wsa);
+    
+    if (err != 0) {
+        throw SocketException("WSAStartup failed: %d", err);
+    }
+    
+    ZeroMemory(&m_hints, sizeof(m_hints));
+    
+    m_hints.ai_family	= m_af;
+    m_hints.ai_socktype = m_type;
+    m_hints.ai_protocol = IPPROTO(m_type);
+    m_hints.ai_flags	= AI_PASSIVE;
+    
+    err = getaddrinfo(NULL, std::to_string(m_port).c_str(), &m_hints, &m_result);
+    
+    if (err != 0) {
+        disconnect();
+        throw SocketException("getaddrinfo failed: %d", err);
+    }
+    
+    m_socket = socket(m_result->ai_family, m_result->ai_socktype, m_result->ai_protocol);
+    
+    if (m_socket == INVALID_SOCKET) {
+        disconnect();
+        throw SocketException("socket failed with error: %ld", WSAGetLastError());
+    }
+    
+#endif
+}
+
+/**
+ * Establishes a client connection.
+ */
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+void Socket<socket_t, service_t>::connect_client() {
+    /**
+     * Throw an exception if the port hasn't be defined by the user.
+     */
+    if(!m_port) {
+        throw SocketException("port isn't defined");
+    }
+    
+    /**
+     * Throw an exception if the hostname hasn't be defined by the user.
+     */
+    if(m_hostname.empty()) {
+        throw SocketException("hostname isn't defined");
+    }
+    
+#if defined(__NIX)
+    m_socket = socket(m_af, m_type, IPPROTO(m_type));
+    
+    if(m_socket == -1) {
+        throw SocketException("socket_failed: %s", strerror(errno));
+    }
+    
+    /**
+     * So that we can re-bind to it without TIME_WAIT problems
+     */
+    int ra = 1;
+    int rc = setsockopt(m_socket, SOL_SOCKET, SO_REUSEADDR, &ra, sizeof(ra));
+    
+    if(rc == -1) {
+        throw SocketException("setsockopt failed: %s", strerror(errno));
+    }
+    
+    set_blocking(m_socket, false);
+    
+    m_host = gethostbyname(m_hostname.c_str());
+    
+    if(m_host == NULL) {
+        throw SocketException("gethostbyname failed: %s", hstrerror(h_errno));
+    }
+    
+    /**
+     * Zero out m_sockaddr struct and then copy the
+     * host address to it's sin_addr member variable.
+     */
+    memset(&m_sockaddr, 0, sizeof(struct sockaddr_in));
+    bcopy((char*)m_host->h_addr, (char*)&m_sockaddr.sin_addr, m_host->h_length);
+    
+    /**
+     * Set address family and port.
+     */
+    m_sockaddr.sin_family = m_af;
+    m_sockaddr.sin_port   = htons(m_port);
+    
+    /**
+     * TCP
+     */
+    if(m_type == SOCK_STREAM) {
+        if(::connect(m_socket, (struct sockaddr*)&m_sockaddr, sizeof(struct sockaddr_in)) == -1) {
+            /*if(errno == EINPROGRESS) {
+             FD_ZERO(&m_active_fd_set);
+             FD_SET(m_socket, &m_active_fd_set);
+             
+             if (select(m_socket + 1, NULL, &m_active_fd_set, NULL, &m_time) > 0) {
+             socklen_t lon = sizeof(int);
+             int optval;
+             getsockopt(m_socket, SOL_SOCKET, SO_ERROR, &optval, &lon);
+             
+             if(optval) {
+             throw SocketException("getsockopt failed: %s", strerror(optval));
+             }
+             }
+             }
+             else {
+             throw SocketException("connect failed: %s", strerror(errno));
+             }*/
+        }
+        else {
+            throw SocketException("connect failed: %s", strerror(errno));
+        }
+    }
+    
+#else
+    /**
+     * @TODO: implement winsock
+     */
+#endif
+}
+
+/**
+ *
+ */
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+bool Socket<socket_t, service_t>::ready(const uint32_t& events) {
+    
+    if(!(m_pfd->events & events)) {
+        return false;
+    }
+    
+    return true;
+}
+
+/**
+ *
+ */
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+bool Socket<socket_t, service_t>::set_blocking(int fd, bool blocking) {
+    
+    if(fd < 0) {
+        return false;
+    }
+    
+    uint32_t rc = -1;
+    
+#ifdef __WIN
+    uint32_t mode = blocking ? 0 : 1;
+    rc   = ioctlsocket(fd, FIONBIO, &mode)
+    
+#else
+    int32_t flags = fcntl(fd, F_GETFL, 0);
+    
+    if(flags < 0) {
+        return false;
+    }
+    
+    flags = (blocking ? (flags & ~O_NONBLOCK) : (flags | O_NONBLOCK));
+    rc    = fcntl(fd, F_SETFL, flags);
+    
+#endif
+    
+    return (rc == 0 ? true : false);
+}
+
+/**
+ *
+ */
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+std::string Socket<socket_t, service_t>::receive() {
+    std::string received;
+    
+#if defined(__NIX)
+    if(m_type == TCP) {
+        char buffer[ m_buf_size ];
+        
+        memset(buffer, 0, m_buf_size);
+        
+        if(m_service_type == SERVER) {
+            socklen_t sock_size = sizeof(struct sockaddr_in);
+            
+            m_socket_tcp = accept(m_socket, (struct sockaddr*)&m_sockaddr, &sock_size);
+            
+            if(m_socket_tcp == -1) {
+                close(m_socket_tcp);
+                throw SocketException("accept_failed: %s", strerror(errno));
+            }
+            
+            m_pfd->fd     = m_socket;
+            m_pfd->events = POLLIN;
+            
+            if(read(m_socket_tcp, &buffer, m_buf_size) == -1) {
+                close(m_socket_tcp);
+                throw SocketException("read_failed: %s", strerror(errno));
+            }
+            
+            received = buffer;
+        }
+        
+        else {
+            ssize_t n = read(m_socket, buffer, m_buf_size);
+            received = buffer;
+            
+            if(n == -1) {
+                close(m_socket);
+                throw SocketException("read_failed: %s", strerror(errno));
+            }
+        }
+    }
+    else if(m_type == UDP) {
+        
+    }
+#else
+    
+#endif
+    
+    return received;
+}
+
+/**
+ *
+ */
+template<SOCKET_TYPE socket_t, SERVICE_TYPE service_t>
+void Socket<socket_t, service_t>::send(const std::string& message) {
+    
+    if(m_socket == -1) {
+        throw SocketException("socket_not_established");
+    }
+    
+#if defined(__NIX)
+    if(m_type == TCP) {
+        int socket = (m_service_type == SERVER ? m_socket_tcp : m_socket);
+        
+        if(socket == -1) {
+            throw SocketException("socket_not_established");
+        }
+        
+        size_t n = write(socket, message.c_str(), message.size());
+        
+        if(n == -1) {
+            throw SocketException("write_failed: %s", strerror(errno));
+        }
+    }
+    else if(m_type == UDP) {
+        char buffer[ m_buf_size ];
+        
+        memset(buffer, 0, m_buf_size);
+        
+        socklen_t sock_len = sizeof(struct sockaddr_in);
+        ssize_t n          = recvfrom(m_socket, buffer, m_buf_size, 0, (struct sockaddr*)&m_sockaddr, &sock_len);
+        
+        if(n == -1) {
+            close(m_socket);
+            throw SocketException("recvfrom_failed: %s", strerror(errno));
+        }
+    }
+#else
+    
+#endif
+}
 
 #endif
