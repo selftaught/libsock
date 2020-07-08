43eee42f4551074f0b6d92d84901734bb70ab40e
Dillan <dillan.hildebrand@protonmail.com>
2020-07-07 21:57:52 -0600

    --allow-empty

---
+++ scripts/crossbuild.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+PLATFORMS=(
+  "x86_64"
+  "arm-linux-gnueabi"
+  "arm-linux-gnueabihf"
+  "powerpc64le-linux-gnu"
+  "aarch64-linux-gnu"
+  "mipsel-linux-gnu"
+  "i386-apple-darwin"
+  "x86_64-apple-darwin"
+  "i686-w64-mingw32"
+  "x86_64-w64-mingw32"
+)
+
+for p in ${PLATFORMS[@]}; do
+  echo -e "Building for: \033[94m${p}\033[0m"
+  docker run --rm -e CROSS_TRIPLE=${p} -v $(PWD)/../:/workdir multiarch/crossbuild /bin/sh -c "./scripts/docker-entrypoint.sh"
+done
